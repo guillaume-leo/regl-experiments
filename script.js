3e64f28562648fd3ef891bbb1eada735a47ea0bd
guillaume-leo <quet.guillaume.leo@gmail.com>
2021-02-09 16:56:01 +0100

    edit README

--- script.js
+++ script.js
@@ -7,43 +7,63 @@ const initialCode = `// In development, you would probably import/require regl
 
 var regl = createREGL(target);
 
+
 var drawTriangle = regl({
 
-  // fragment shader
+  // Shaders in regl are just strings.  You can use glslify or whatever you want
+  // to define them.  No need to manually create shader objects.
   frag: \`
-  precision mediump float;
-  uniform vec4 color;
-  void main () {
-    gl_FragColor = color;
-  }\`,
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }\`,
 
-  // vertex shader
   vert: \`
-  precision mediump float;
-  attribute vec2 position;
-  void main () {
-    gl_Position = vec4(position, 0, 1);
-  }\`,
+    precision mediump float;
+    attribute vec2 position;
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }\`,
 
-  // attributes
+  // Here we define the vertex attributes for the above shader
   attributes: {
-    position: [
-      [-1, 0],
-      [0, -1],
-      [1, 1]
-    ]
+    // regl.buffer creates a new array buffer object
+    position: regl.buffer([
+      [-2, -2],   // no need to flatten nested arrays, regl automatically
+      [4, -2],    // unrolls them into a typedarray (default Float32)
+      [4,  4]
+    ])
+    // regl automatically infers sane defaults for the vertex attribute pointers
   },
 
-  // uniforms
   uniforms: {
-    color: [1, 0, 0, 1]
+    // This defines the color of the triangle to be a dynamic variable
+    color: regl.prop('color')
   },
 
-  // vertex count
+  // This tells regl the number of vertices to draw in this command
   count: 3
 })
 
-drawTriangle();`;
+// regl.frame() wraps requestAnimationFrame and also handles viewport changes
+regl.frame(({time}) => {
+  // clear contents of the drawing buffer
+  regl.clear({
+    color: [0, 0, 0, 0],
+    depth: 1
+  })
+
+  // draw a triangle using the command defined above
+  drawTriangle({
+    color: [
+      Math.cos(time * 1.001),
+      Math.sin(time * 2.8),
+      Math.cos(time * 3.003),
+      1
+    ]
+  })
+})`;
 
 
 
@@ -74,6 +94,7 @@ console.log("code sent");
 function KeyPress(e) {
       var evtobj = window.event? event : e
       if (evtobj.keyCode == 13 && evtobj.ctrlKey){
+        console.clear();
       //REMOVE CHILD AND RECREATE IT
       var element = document.getElementById("target");
       while (element.firstChild) {
@@ -89,58 +110,3 @@ function KeyPress(e) {
 }
 document.onkeydown = KeyPress;
 
-
-
-/*
-// In development, you would probably import/require regl
-// const regl = require('regl')();
-
-// In this block, it is already loaded, so we just
-// initialize it. For more info, see:
-// https://github.com/regl-project/regl#standalone-script-tag
-
-var regl = createREGL(hello_world);
-
-var drawTriangle = regl({
-
-  // fragment shader
-  frag: `
-  precision mediump float;
-  uniform vec4 color;
-  void main () {
-    gl_FragColor = color;
-  }`,
-
-  // vertex shader
-  vert: `
-  precision mediump float;
-  attribute vec2 position;
-  void main () {
-    gl_Position = vec4(position, 0, 1);
-  }`,
-
-  // attributes
-  attributes: {
-    position: [
-      [-1, 0],
-      [0, -1],
-      [1, 1]
-    ]
-  },
-
-  // uniforms
-  uniforms: {
-    color: [1, 0, 0, 1]
-  },
-
-  // vertex count
-  count: 3
-})
-
-drawTriangle();
-
-
-
-
-
-*/
